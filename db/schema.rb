9c17939cf9f8c3a6352595a9ed3bbb25ff48ceb2
Eshaam Rabaney <eshaam.rabaney@gmail.com>
2016-07-27 11:50:29 +0200

    enable uuid

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727092927) do
+ActiveRecord::Schema.define(version: 20160727094954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
