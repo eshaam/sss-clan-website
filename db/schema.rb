f2d17bf231410958a08d9154ab1b60f742157ae7
Eshaam Rabaney <eshaam.rabaney@gmail.com>
2016-07-27 13:25:26 +0200

    add status to highlight to determine which highlights display on homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727095043) do
+ActiveRecord::Schema.define(version: 20160727112431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,18 @@ ActiveRecord::Schema.define(version: 20160727095043) do
     t.text     "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "highlights", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.text     "image"
+    t.string   "title"
+    t.text     "description"
+    t.text     "action_link"
+    t.string   "action_text"
+    t.integer  "order",       default: 0
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "active",      default: false
   end
 
 end
