911f4a3f7824ee821fb0fa5d12fca6e9ca68dffd
Eshaam Rabaney <eshaam.rabaney@gmail.com>
2016-07-27 12:32:50 +0200

    implement games on the frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :games, only: [:index,:show]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
