1831b4af810d23d1dbad6c7736b1ab6d34ec0f7c
Eshaam Rabaney <eshaam.rabaney@gmail.com>
2016-08-01 14:53:11 +0200

    Add function to disable and enable members

--- app/admin/member.rb
+++ app/admin/member.rb
@@ -12,16 +12,38 @@ ActiveAdmin.register Member do
     column :psn_gamertag
     column :email
     column :last_sign_in_at
+    column :status
     actions defaults: true
   end
 
+  member_action :disable, method: 'get' do
+    member = Member.friendly.find(params[:id])
+    member.status = false
+    member.save!
+    redirect_to admin_member_path(member), notice: 'member disabled!'
+  end
+  member_action :enable, method: 'get' do
+    member = Member.friendly.find(params[:id])
+    member.status = true
+    member.save!
+    redirect_to admin_member_path(member), notice: 'member enabled!'
+  end
+
+  action_item :only => :show do
+    if member.status == true
+      link_to 'Disable', disable_admin_member_path(member)
+    else
+      link_to 'Enable', enable_admin_member_path(member)
+    end
+  end
 
 
   form do |f|
     f.inputs "Member Details" do
-        f.input :psn_gamertag
-        f.input :email
+
         if f.object.new_record?
+            f.input :psn_gamertag
+            f.input :email
             f.input :password
             f.input :password_confirmation
         end
@@ -29,7 +51,7 @@ ActiveAdmin.register Member do
     f.button :Submit
   end
 
-  permit_params :psn_gamertag, :email,:password, :password_confirmation
+  permit_params :psn_gamertag, :email,:password, :password_confirmation,:status
 
 
 
